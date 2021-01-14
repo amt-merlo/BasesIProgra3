9c328a0faf157f751a65eb62245dae378b1d7821
amt-merlo <amt.merlo@gmail.com>
2021-01-13 21:57:43 -0600

    While para iterar sobre fechas y cuentas

--- Querys/simulacion1.sql
+++ Querys/simulacion1.sql
@@ -33,11 +33,37 @@
 			@HI AS INT, --tope
 			@actual as date --esta es la fecha sobre la que estamos iterando
 
+	--se definen los valores iniciales
 	SET @LO = 1
 	SELECT @HI = COUNT (*) FROM #Temporal 
+	SELECT @actual = Fecha from #Temporal WHERE ID = 1
 
 	WHILE @LO <= @HI
 	BEGIN 
-		SELECT @actual = Fecha from #Temporal WHERE ID = @LO
+		
+		--Hacemos otro while para recorrer todas las cuentas objetivo
+		DECLARE @contador as int, @tope as int
+		SET @contador = 1
+		SELECT @tope = COUNT (*) FROM dbo.CuentaObjetivo
+
+		WHILE @contador <= @tope
+		BEGIN
+			--se saca la fecha fin del ahorro
+			DECLARE @fechaFin as date
+			SELECT @fechaFin = FechaFin from dbo.CuentaObjetivo WHERE ID = @contador
+
+			--si la fecha fin coincide con el día actual entonces se procesa el cierre del ahorro
+			IF @fechaFin = @actual
+				--aqui se procesa todo
+			ELSE
+				--aqui se hacen los calculos de intereses del día
+
+			--Se aumenta el contador del while de cuentas de ahorro
+			SET @contador = @contador +1
+		END
+
+
+		--se le suma un dia a la fecha actual para la siguiente iteración
+		SELECT @actual = DATEADD(DAY, 1, @actual)
 		SET @LO = @LO + 1
 	END 
