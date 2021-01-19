c0b67d09f119a03f554b1c091dcc6f217b00a4f8
amt-merlo <amt.merlo@gmail.com>
2021-01-19 17:12:00 -0600

    Proceso final de redencion en CO
    Se realiza correctamente la redención de los intereses acumulados en la cuenta objetivo y se procesa la redención total del ahorro

--- Querys/simulacion1.sql
+++ Querys/simulacion1.sql
@@ -51,7 +51,6 @@
 		WHILE @contador <= @tope
 		BEGIN
 
-			
 			--se saca la fecha fin del ahorro
 			DECLARE @fechaFin as date
 			SELECT @fechaFin = FechaFin from dbo.CuentaObjetivo WHERE ID = @contador
@@ -60,90 +59,149 @@
 			DECLARE @fechaInicio as date 
 			SELECT @fechaInicio = FechaInicio FROM dbo.CuentaObjetivo WHERE ID = @contador
 
-			DECLARE @cantidadMeses as int
-			SELECT @cantidadMeses = DATEDIFF(MONTH, @fechaInicio, @fechaFin)
+			IF @actual BETWEEN @fechaInicio AND @fechaFin
+			BEGIN
+				DECLARE @cantidadMeses as int
+				SELECT @cantidadMeses = DATEDIFF(MONTH, @fechaInicio, @fechaFin)
 
-			DECLARE @porcentajeInteres as float
-			SET @porcentajeInteres = @cantidadMeses * 0.5
+				DECLARE @porcentajeInteres as float
+				SET @porcentajeInteres = @cantidadMeses * 0.5
 
-			--Se calcula el interés diario
-			SET @porcentajeInteres = @porcentajeInteres / 365
+				--Se calcula el interés diario
+				SET @porcentajeInteres = @porcentajeInteres / 365
 			
-			--Se le aplica el interés ganado a la cuenta objetivo
-			UPDATE dbo.CuentaObjetivo
-			SET InteresAcum = InteresAcum + (Saldo * @porcentajeInteres) / 100
-			WHERE ID = @contador
+				--Se calcula el interés ganado
+				DECLARE @interesGanado as float, @saldoCO as float, @interesAnterior as float
+				SELECT @interesAnterior = InteresAcum FROM CuentaObjetivo WHERE ID = @contador
+				SELECT @saldoCO = Saldo FROM CuentaObjetivo WHERE ID = @contador
+				SET @interesGanado = (@saldoCO * @porcentajeInteres) / 100
 			
-			--PROCESAR REDENCIÓN DE CO-- 
-			IF @fechaFin = @actual
-				--aqui se procesa todo
-				SELECT * FROM dbo.CuentaObjetivo
-			ELSE
-				--aqui se pregunta si es el día del ahorro del mes 
-				DECLARE @diaAhorro as int
-				SELECT @diaAhorro = DiasDeposito FROM CuentaObjetivo WHERE ID = @contador
 
-				--sacamos el día de la fecha actual
-				DECLARE @diaActual as int
-				SELECT @diaActual = DAY(@actual)
+				--Se le aplica el interés ganado a la cuenta objetivo
+				UPDATE dbo.CuentaObjetivo
+				SET InteresAcum = InteresAcum + @interesGanado
+				WHERE ID = @contador
+			
+				--Creamos el movimiento de la cuenta movCOInteres--
+				INSERT INTO dbo.MovCOInteres (CuentaObjetivoID, Fecha, Monto, NuevoInteresAcum)
+				VALUES (@contador, @actual, @interesGanado, @interesAnterior+@interesGanado)
 
-				IF @diaAhorro = @diaActual 
+
+				--PROCESAR REDENCIÓN DE CO-- 
+				IF @fechaFin = @actual
 				BEGIN
-					--PROCESAR DEPÓSITOS EN LA CO--
+					--***Redención de intereses***--
+				
+					--sacamos el total de intereses acumulados
+					DECLARE @totalInteres as float
+					SELECT @totalInteres = InteresAcum FROM dbo.CuentaObjetivo WHERE ID = @contador
+				
+
+					UPDATE dbo.CuentaObjetivo
+					SET InteresAcum = 0.0
+					WHERE ID = @contador
+
+					--Creamos el movimiento en la tabla MovCOInteres
+					INSERT INTO dbo.MovCOInteres(CuentaObjetivoID, Fecha, Monto, NuevoInteresAcum)
+					VALUES(@contador, @actual, @totalInteres, 0)
+
+					--Creamos el movimiento en la tabla MovCo
+					INSERT INTO dbo.MovCO(TipoMovCOID, Fecha, Monto)
+					VALUES(2, @actual, @totalInteres)
+
+					----Sumamos el interés acumulado al saldo de la CO
+					UPDATE dbo.CuentaObjetivo
+					SET Saldo = Saldo + @totalInteres
+					WHERE ID = @contador
+
+					----***Redención de CO***--
+					DECLARE @totalAhorrado as float
+					SELECT @totalAhorrado = Saldo FROM dbo.CuentaObjetivo
+					WHERE ID = @contador
 					
-					--primero se corrobora que la cuenta de ahorro tiene saldo suficiente para debitar el monto
-					DECLARE @SaldoRestante as float --aqui se guarda el restante de la CA después de debitar el monto del ahorro
-					DECLARE @montoAhorro as float
-					DECLARE @saldoCA as float
-					DECLARE @numCuenta as int
 
-					SELECT @montoAhorro = Cuota FROM CuentaObjetivo WHERE ID = @contador
-					SELECT @numCuenta = CuentadeAhorroID FROM CuentaObjetivo WHERE ID = @contador
+					UPDATE dbo.CuentaObjetivo
+					SET Saldo = 0
+					WHERE ID = @contador
 
-					SELECT @saldoCA = Saldo FROM CuentadeAhorro WHERE NumerodeCuenta = @numCuenta
+					--Buscamos el id de la Cuenta de Ahorros
+					DECLARE @CuentaAhorrosID as int
+					SELECT @CuentaAhorrosID = CuentadeAhorroID FROM dbo.CuentaObjetivo WHERE ID = @contador 
 
-					SET @SaldoRestante = @saldoCA - @montoAhorro
+					--sumamos el ahorro al saldo de la cuenta principal
+					UPDATE dbo.CuentadeAhorro
+					SET Saldo = Saldo + @totalAhorrado
+					WHERE NumerodeCuenta = @CuentaAhorrosID
+				   
+					--Tomamos el saldo de la CA
+					DECLARE @SaldoCuentaAhorros as float
+					SELECT @SaldoCuentaAhorros = Saldo FROM dbo.CuentadeAhorro WHERE NumerodeCuenta = @CuentaAhorrosID
 					
-					--se hace el rebajo en la CA
-					IF @SaldoRestante >= 0 
-						UPDATE dbo.CuentadeAhorro
-						SET Saldo = @SaldoRestante
-						WHERE NumerodeCuenta = @numCuenta
+					--Registramos el movimiento en la tabla de MovimientoCA
+					INSERT INTO dbo.MovimientoCA(TipoMovCAID, numCuentaID, Fecha, Monto, NuevoSaldo, Descripcion, Visible)
+					VALUES(4, @CuentaAhorrosID, @actual, @totalAhorrado, @SaldoCuentaAhorros, 'Ahorro de Cuenta Objetivo', 1)
+
+				END	
+				ELSE
+					--aqui se pregunta si es el día del ahorro del mes 
+					DECLARE @diaAhorro as int
+					SELECT @diaAhorro = DiasDeposito FROM CuentaObjetivo WHERE ID = @contador
+
+					--sacamos el día de la fecha actual
+					DECLARE @diaActual as int
+					SELECT @diaActual = DAY(@actual)
+
+					IF @diaAhorro = @diaActual 
+					BEGIN
+						--PROCESAR DEPÓSITOS EN LA CO--
+					
+						--primero se corrobora que la cuenta de ahorro tiene saldo suficiente para debitar el monto
+						DECLARE @SaldoRestante as float --aqui se guarda el restante de la CA después de debitar el monto del ahorro
+						DECLARE @montoAhorro as float
+						DECLARE @saldoCA as float
+						DECLARE @numCuenta as int
+
+						SELECT @montoAhorro = Cuota FROM CuentaObjetivo WHERE ID = @contador
+						SELECT @numCuenta = CuentadeAhorroID FROM CuentaObjetivo WHERE ID = @contador
+
+						SELECT @saldoCA = Saldo FROM CuentadeAhorro WHERE NumerodeCuenta = @numCuenta
+
+						SET @SaldoRestante = @saldoCA - @montoAhorro
+					
+						--se hace el rebajo en la CA
+						IF @SaldoRestante >= 0 
+							UPDATE dbo.CuentadeAhorro
+							SET Saldo = @SaldoRestante
+							WHERE NumerodeCuenta = @numCuenta
 						
-						--creamos un nuevo movimiento para reflejar el debito en la CA
-						INSERT INTO dbo.MovimientoCA (TipoMovCAID, 
-													  numCuentaID, 
-													  Fecha, 
-													  Monto, 
-													  NuevoSaldo, 
-													  Descripcion, 
-													  Visible)
+							--creamos un nuevo movimiento para reflejar el debito en la CA
+							INSERT INTO dbo.MovimientoCA (TipoMovCAID, 
+														  numCuentaID, 
+														  Fecha, 
+														  Monto, 
+														  NuevoSaldo, 
+														  Descripcion, 
+														  Visible)
 
-						VALUES(3, @numCuenta, @actual, @montoAhorro, @SaldoRestante, 'Ahorro', 1)
-
-
-						--creamos un nuevo movimiento para reflejar el crédito en la CO	
-						INSERT INTO dbo.MovCO (TipoMovCOID, 
-											   Fecha, 
-											   Monto) 
-
-						VALUES (1, @actual, @montoAhorro)
-
-						--Se hace el incremento en el saldo la CO
-						UPDATE dbo.CuentaObjetivo
-						SET Saldo = Saldo + @montoAhorro
-						WHERE ID = @contador
+							VALUES(3, @numCuenta, @actual, @montoAhorro, @SaldoRestante, 'Ahorro', 1)
 
 
+							--creamos un nuevo movimiento para reflejar el crédito en la CO	
+							INSERT INTO dbo.MovCO (TipoMovCOID, 
+												   Fecha, 
+												   Monto) 
+
+							VALUES (1, @actual, @montoAhorro)
+
+							--Se hace el incremento en el saldo la CO
+							UPDATE dbo.CuentaObjetivo
+							SET Saldo = Saldo + @montoAhorro
+							WHERE ID = @contador
 
 
-
-
-
-					
-			END
+				END
 			
-
+			END
 			--Se aumenta el contador del while de cuentas de ahorro
 			SET @contador = @contador +1
 		END
